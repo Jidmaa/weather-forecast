56765a7454c6cb39aaf9d77e33a208f59cca3841
Jidmaa <majidbenhenneda@gmail.com>
2021-06-03 18:30:59 +0100

    added leaflet map component boilerplate, starting on the design

--- src/API/open_weather.instance.js
+++ src/API/open_weather.instance.js
@@ -2,7 +2,17 @@ import axios from "axios";
 export const getWeather = () => {
   return axios
     .get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&lang=fr&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=28&lon=3&exclude=minutely&lang=fr&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+    )
+    .then((res) => {
+      console.log(res);
+      return res;
+    });
+};
+export const getCityByName = (city) => {
+  return axios
+    .get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
     )
     .then((res) => {
       console.log(res);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { getWeather } from "./API/open_weather.instance";
+import { getWeather, getCityByName } from "./API/open_weather.instance";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 function App() {
-  getWeather();
-  return <h1> Hello world</h1>;
+  return (
+    <>
+      <div className="main_frame">
+        <div className="left_side"></div>
+        <div className="right_side"></div>
+      </div>
+
+      {/* <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer> */}
+    </>
+  );
 }
 
 export default App;
