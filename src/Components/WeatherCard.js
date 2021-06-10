2ded24c41e09a6f79a141cfcbe17325a6df87105
Jidmaa <majidbenhenneda@gmail.com>
2021-06-10 07:03:59 +0100

    refactoring

---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,74 @@
+import { FiEye, FiDroplet, FiWind, FiThermometer } from "react-icons/fi";
+import Lottie from "react-lottie";
+import animationData from "../sunny.json";
+export default function WeatherCard({ currentWeather }) {
+  const animations = [
+    {
+      name: "Sunny",
+      options: {
+        animationData,
+        loop: true,
+        autoplay: true,
+        rendererSettings: {
+          preserveAspectRatio: "xMaxYMax slice",
+        },
+      },
+    },
+  ];
+  return (
+    <div className="card">
+      <div className="weather-today">
+        <Lottie
+          options={animations[0].options}
+          height={150}
+          width={150}
+          style={{ margin: "unset" }}
+        />
+        <div className="weather">
+          <div className="weather-state">
+            <span>{currentWeather?.current?.weather[0]?.main}</span>
+          </div>
+
+          {/* <span className="weather-state">
+        {currentWeather?.current?.temp} °C,{" "}
+        {currentWeather?.current?.weather[0].description}.
+      </span>
+      Feels like {currentWeather?.current?.feels_like} °C. */}
+          {currentWeather && (
+            <>
+              {" "}
+              <div className="temp">
+                {Math.floor(currentWeather?.current?.temp)} °C
+              </div>
+              <span>
+                {" "}
+                Feels like {Math.floor(
+                  currentWeather?.current?.feels_like
+                )} °C.{" "}
+              </span>{" "}
+            </>
+          )}
+        </div>
+      </div>
+
+      <div className=" weather-properties small">
+        <span className="weather-property">
+          <FiDroplet className="property-icon" /> Humidity :{" "}
+          {currentWeather?.current.humidity} %{" "}
+        </span>
+        <span className="weather-property">
+          <FiWind className="property-icon" /> Wind speed :{" "}
+          {Math.floor(currentWeather?.current.wind_speed)} m/s{" "}
+        </span>
+        <span className="weather-property">
+          <FiEye className="property-icon" /> Visibility :{" "}
+          {(currentWeather?.current?.visibility / 1000).toFixed(1)} Km
+        </span>
+        <span className="weather-property">
+          <FiThermometer className="property-icon" /> Dew point :{" "}
+          {Math.floor(currentWeather?.current?.dew_point)} °C
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/Components/WeatherState.js
+++ src/Components/WeatherState.js
@@ -1,23 +1,5 @@
-import { getWeather, getCityByName } from "../API/open_weather.instance";
-import Lottie from "react-lottie";
-import animationData from "../sunny.json";
-
-import { FiEye, FiDroplet, FiWind, FiThermometer } from "react-icons/fi";
+import WeatherCard from "./WeatherCard";
 export default function WeatherState({ currentWeather }) {
-  const animations = [
-    {
-      name: "Sunny",
-      options: {
-        animationData,
-        loop: true,
-        autoplay: true,
-        rendererSettings: {
-          preserveAspectRatio: "xMaxYMax slice",
-        },
-      },
-    },
-  ];
-
   console.log("current weather in weatherstate", currentWeather);
   const makeDate = () => {
     var dateObj = new Date();
@@ -47,61 +29,9 @@ export default function WeatherState({ currentWeather }) {
       <div>
         <h1> Today </h1>
       </div>
-      <div className="card">
-        <div className="weather-today">
-          <Lottie
-            options={animations[0].options}
-            height={150}
-            width={150}
-            style={{ margin: "unset" }}
-          />
-          <div className="weather">
-            <div className="weather-state">
-              <span>{currentWeather?.current?.weather[0]?.main}</span>
-            </div>
-
-            {/* <span className="weather-state">
-            {currentWeather?.current?.temp} °C,{" "}
-            {currentWeather?.current?.weather[0].description}.
-          </span>
-          Feels like {currentWeather?.current?.feels_like} °C. */}
-            {currentWeather && (
-              <>
-                {" "}
-                <div className="temp">
-                  {Math.floor(currentWeather?.current?.temp)} °C
-                </div>
-                <span>
-                  {" "}
-                  Feels like {Math.floor(
-                    currentWeather?.current?.feels_like
-                  )}{" "}
-                  °C.{" "}
-                </span>{" "}
-              </>
-            )}
-          </div>
-        </div>
-
-        <div className=" weather-properties small">
-          <span className="weather-property">
-            <FiDroplet style={{ fontSize: "1rem", marginRight: ".5rem" }} />{" "}
-            Humidity : {currentWeather?.current.humidity} %{" "}
-          </span>
-          <span className="weather-property">
-            <FiWind style={{ fontSize: "1rem", marginRight: ".5rem" }} /> Wind
-            speed : {Math.floor(currentWeather?.current.wind_speed)} m/s{" "}
-          </span>
-          <span className="weather-property">
-            <FiEye style={{ fontSize: "1rem", marginRight: ".5rem" }} />{" "}
-            Visibility :{" "}
-            {(currentWeather?.current?.visibility / 1000).toFixed(1)} Km
-          </span>
-          <span className="weather-property">
-            <FiThermometer style={{ fontSize: "1rem", marginRight: ".5rem" }} />{" "}
-            Dew point : {Math.floor(currentWeather?.current?.dew_point)} °C
-          </span>
-        </div>
+      <WeatherCard currentWeather={currentWeather} />
+      <div>
+        <h1> This week </h1>
       </div>
     </div>
   );
